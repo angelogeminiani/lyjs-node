9b3e2c1eb0aa0b28ace109a79e5725cf289436f8
gian angelo geminiani <angelo.geminiani@gmail.com>
2014-05-08 14:41:44 +0200

    no message

--- index.js
+++ index.js
@@ -9,11 +9,17 @@
     //-- utility --//
     module.exports.ly = require('./lib/ly');
 
+    //-- base classes (extend from this if you want those features) --//
+    module.exports.Invoker = require('./lib/base/invoker');
+    module.exports.Events = require('./lib/base/events');
+    module.exports.Template = require('./lib/base/template');
+
     //-- File System --//
     module.exports.path = require('./lib/fs/path');
     module.exports.fs = require('./lib/fs/fs');
     module.exports.UploadManager = require('./lib/fs/upload-manager');
 
-
+    //-- Forms --//
+    module.exports.forms = require('./lib/form/form-utils');
 
 })();
